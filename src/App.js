eabd6fe7c98dc52361af5d7f0dc6d6e42e779add
mauliukani <mauliuakni@gmail.com>
2025-12-04 06:05:45 +0530

    My react Js Website

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Content";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav style={{ padding: "10px", background: "#eee" }}>
+      <h2>My React Website</h2>
+    </nav>
+  );
+}
+
+export default Navbar;
